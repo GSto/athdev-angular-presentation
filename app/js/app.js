ce961e5efd97ea5f6aeb893eb060f0d2ee0594b9
Glenn Stovall <glennstovall@gmail.com>
2013-07-01 18:35:18 -0400

    finished product

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,19 @@
-'use strict';
+todo = angular.module('todo', []);
 
+todo.controller('mainCtrl', function($scope) {
+	$scope.task = {
+		name : '',
+		complete: false
+	};
+
+	$scope.tasks = [];
+
+	$scope.addTask = function() {
+		$scope.tasks.push($scope.task);
+		$scope.task = { 
+			name : '',
+			complete: false
+		};
+	}
+
+});
